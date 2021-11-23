4523cb87628d981cd67a9ec1b4f534d4059b9620
Azraar Azward <azraar@dolphiq.nl>
2021-11-23 08:49:40 +0530

    Add extensions related changes and bug fixes

--- frontend/controllers/PostsController.php
+++ frontend/controllers/PostsController.php
@@ -12,24 +12,24 @@ class PostsController extends Controller
 
     public function actionIndex()
     {
-        $posts = Posts::find()->where(['status'=>1])->andWhere('posted_by != 0 and posted_by != ""');
+        $posts = Posts::find()->where(['status' => 1])->andWhere('posted_by != 0 and posted_by != ""');
         $countQuery = clone $posts;
-        $pages = new Pagination(['totalCount'=>$countQuery->count()]);
+        $pages = new Pagination(['totalCount' => $countQuery->count()]);
         $models = $posts->offset($pages->offset)->limit($pages->limit)->all();
 
-        return $this->render('index',['models'=>$models,'pages'=>$pages]);
+        return $this->render('index', ['models' => $models, 'pages' => $pages]);
     }
 
     public function actionPost($id)
     {
         $post = Posts::findOne(['id' => $id]);
-        return $this->render('post',['post'=>$post]);
+        return $this->render('post', ['post' => $post]);
     }
 
     public function actionUser($id)
     {
-        $user = User::findOne(['id'=>$id]);
-        return $this->render('user',['user'=>$user]);
+        $user = User::findOne(['id' => $id]);
+        return $this->render('user', ['user' => $user]);
     }
 
 }
